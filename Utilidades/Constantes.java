6f1b8bbc7eaa5c75e3685384382538a22e66a840
moraleso3 <51491725+moraleso3@users.noreply.github.com>
2019-06-07 20:35:06 -0600

    Add files via upload

---
+++ Utilidades/Constantes.java
@@ -0,0 +1,12 @@
+package com.chat.app.Utilidades;
+
+/**
+ * Created by user on 30/08/2018. 30
+ */
+public class Constantes {
+
+    public static final String URL_FOTO_POR_DEFECTO_USUARIOS = "https://firebasestorage.googleapis.com/v0/b/proyectofinal2-fea05.appspot.com/o/Fotos%2FFotoPerfil%2Fdefault_profile.png?alt=media&token=79520bd8-b881-4864-b8be-a695589b9518";
+    public static final String NODO_USUARIOS = "Usuarios";
+    public static final String NODO_MENSAJES = "Mensajes";
+
+}
